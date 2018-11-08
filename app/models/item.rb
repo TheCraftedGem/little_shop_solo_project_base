f6f0679e760ef5ff7e6d23d0b5fc63902752fea2
Emilio <thecraftedgem@tuta.io>
2018-11-07 17:53:33 -0700

    UI added for discounts

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  include ActionView::Helpers::NumberHelper
   belongs_to :user
   has_many :order_items
   has_many :orders, through: :order_items
@@ -28,12 +29,14 @@ class Item < ApplicationRecord
     discounts.count >= 1
   end
 
-  def apply_discount(cart_item_count)
+  def apply_discount(amount_purchased)
     if check_for_discount
-      if discounts.where("quantity <= #{cart_item_count}").count > 0
-        discount = discounts.where("quantity <= #{cart_item_count}").order(:quantity).reverse
+      if discounts.where("quantity <= #{amount_purchased}").count > 0
+        discount = discounts.where("quantity <= #{amount_purchased}").order(:quantity).reverse
         discount_rate = 1 - discount.first.rate/100.0
         final_price = price * discount_rate
+      else
+        self.price
       end
     else
       self.price
